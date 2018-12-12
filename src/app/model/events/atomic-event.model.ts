6411bbe68cc2e96456361d0217443a75e8d411bc
Ralph Gasser <ralph.gasser@unibas.ch>
2018-12-12 10:02:34 +0100

    Added comment.

--- src/app/model/events/atomic-event.model.ts
+++ src/app/model/events/atomic-event.model.ts
@@ -1,6 +1,10 @@
 import {EventCategory} from './event-category.model';
 import {SubmittedEvent} from './event.model';
 
+
+/**
+ * An AtomicEvent.
+ */
 export interface AtomicEvent extends SubmittedEvent {
     category: EventCategory;
     type: string;
